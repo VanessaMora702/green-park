5311317e5b6bdbca9d61db426862d92ea272ab55
vanessaMora702 <vanessamora702@gmail.com>
2020-10-28 17:26:59 -0400

    Passed the store to the main.js file

--- admin-dashboard/src/main.js
+++ admin-dashboard/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './quasar'
+import store from './vuexStore/index';
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
